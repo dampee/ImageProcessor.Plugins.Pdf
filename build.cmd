52196489889f9fc5994326bb7976d85057b0d8c5
Damiaan <damiaan@comm-it.be>
2017-04-24 15:16:31 +0200

    Make a multi-framework targeted nuget package

--- build.cmd
+++ build.cmd
@@ -1,1 +1,4 @@
-nuget pack
+REM nuget pack
+
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" ImageProcessor.Plugins.Pdf.csproj /t:Build /p:Configuration="Release45"
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" ImageProcessor.Plugins.Pdf.csproj /t:Build;Package;Publish /p:Configuration="Release461"
