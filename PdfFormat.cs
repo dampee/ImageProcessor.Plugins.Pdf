00ca1bd4633ad36f89f1e831b3d6fde860b84469
Damiaan <damiaan@comm-it.be>
2017-04-24 14:58:22 +0200

    Add comments for public class and methods

--- PdfFormat.cs
+++ PdfFormat.cs
@@ -7,19 +7,47 @@ using ImageProcessor.Imaging.Formats;
 
 namespace ImageProcessor.Plugins.Pdf
 {
+    /// <summary>
+    /// The plugin enabling imageprocessor to read PDF as an image source
+    /// </summary>
     public class PdfFormat : FormatBase
     {
+        /// <summary>
+        /// Every PDF file starts with %PDF
+        /// </summary>
+        /// <remarks>
+        /// not according the specs, but in reality it does
+        /// </remarks>
         public override byte[][] FileHeaders => new[]
         {
             Encoding.UTF8.GetBytes("%PDF") // Must be 4 bytes or less in the current version of ImageProcessor
         };
 
+        /// <summary>
+        /// the default extention of a PDF file is "PDF"
+        /// </summary>
         public new string DefaultExtension => "pdf";
 
+        /// <summary>
+        /// All the extensions which are supported
+        /// </summary>
         public override string[] FileExtensions => new[] {"pdf"};
+
+        /// <summary>
+        /// the mimetype of a pdf source
+        /// </summary>
         public override string MimeType => "application/pdf";
+
+        /// <summary>
+        /// The default output format
+        /// </summary>
         public override ImageFormat ImageFormat => ImageFormat.Png;
 
+        /// <summary>
+        /// This method is used to load the pdf into the current stream
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
         public override Image Load(Stream stream)
         {
             var bytes = new byte[stream.Length];
